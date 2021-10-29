ee30f0b371b28b4fe48af81669cc500ef5ff3a14
anoop <anoop@DESKTOP-M7Q8QKP>
2021-10-29 15:50:03 +0530

    Add project files.

---
+++ simpleCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using simpleCalculator.Models;
+
+namespace simpleCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Calculator()
+        {
+            return View(new Calculator());
+        }
+        [HttpPost]
+        public JsonResult Add(int num1, int num2)
+        {
+            int num3 = num1 + num2;
+            return Json(num3);
+        }
+        public JsonResult Sub(int num1, int num2)
+        {
+            int num3 = num1 - num2;
+            return Json(num3);
+        }
+        public JsonResult Mul(int num1, int num2)
+        {
+            int num3 = num1 * num2;
+            return Json(num3);
+
+        }
+        public JsonResult Div(int num1, int num2)
+        {
+            int num3 = num1 / num2;
+            return Json(num3);
+        }
+        public JsonResult Mod(int num1, int num2)
+        {
+            int num3 = num1 % num2;
+            return Json(num3);
+        }
+
+    }
+}
---
+++ simpleCalculator/Models/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace simpleCalculator.Models
+{
+    public class Calculator
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int num3 { get; set; }
+    }
+}
